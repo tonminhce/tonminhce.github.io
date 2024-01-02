3c715efd432c06fc2f8923a9b348715ad2991fce
Minh Nguyễn <87883380+tonminhce@users.noreply.github.com>
2024-01-02 14:39:39 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,10 @@
+// next.config.js
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
   output: 'export',
- 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+  // other configurations...
 }
- 
+
 module.exports = nextConfig
